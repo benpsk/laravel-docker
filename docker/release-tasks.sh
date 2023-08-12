ffe373f56ac43b1509dc786869bd4f9d06516e80
Ben Gunn <bengunn.dev@gmail.com>
2023-08-13 00:29:28 +0630

    update for laravel 10 vite npm run build

--- docker/release-tasks.sh
+++ docker/release-tasks.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
-ROLE=${ROLE:-role}
+ROLE=${ROLE:-app}
 APPENV=${APPENV:-production}
 
 if [ $ROLE == "app" ]; then
-	if [ $APPENV == "production" ]; then
-		echo "Running production tasks."
-		php /usr/bin/composer install --optimize-autoloader --no-dev
-		php artisan optimize
-	else
-		echo "Running tasks."
-		php /usr/bin/composer install
-		php artisan optimize:clear
-	fi
+    if [ $APPENV == "production" ]; then
+        echo "Running production tasks."
+        php /usr/bin/composer install --optimize-autoloader --no-dev
+        php artisan optimize:clear
+        php artisan optimize
+
+        echo "Running assets."
+        npm install --omit=dev
+        npm run build
+
+    else
+        echo "Running tasks."
+        php /usr/bin/composer install
+        php artisan optimize:clear
+
+        echo "Running assets."
+        npm install
+        npm run dev --host=0.0.0.0 &
+    fi
 fi
 
 chown -R www-data:www-data storage
